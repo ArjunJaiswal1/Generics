659553cfa2f5f22ead03d4c9af7f7d3497e4ca5f
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-07 19:11:59 +0530

    [Arjun] Add ; Added Executed

--- Generics Problem/Program.cs
+++ Generics Problem/Program.cs
@@ -4,12 +4,18 @@ class Problem
     static void Main(string[] args)
     {
         //Console.WriteLine("Find the Maximum Number!");
+        Console.WriteLine("Find the Maximum Number!");
         //int[] intArray = { 1, 2, 9, 4 };
         //Maximum<int> generic = new Maximum<int>();
         //generic.PrintMaxValue(intArray);
 
-        double[] doubleArray = { 1.1, 1.2, 1.3 };
-        Maximum<double> genericDouble = new Maximum<double>();
-        genericDouble.PrintMaxValue(doubleArray);
+        //double[] doubleArray = { 1.1, 1.2, 1.3 };
+        //Maximum<double> genericDouble = new Maximum<double>();
+        //genericDouble.PrintMaxValue(doubleArray);
+
+        string[] stringArray = { "Appale", "banana", "peach" };
+        Maximum<string> genericString = new Maximum<string>();
+        genericString.PrintMaxValue(stringArray);
+
     }
 }
