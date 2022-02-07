3e92d8e96e7b91f8fc22c4495ccdba0e4b626072
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-07 19:00:28 +0530

    [Arjun] Add ; Added Switched

---
+++ Generics Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿using GenericsProblem;
+class Problem
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Find the Maximum Number!");
+        int[] intArray = { 1, 2, 9, 4 };
+        Maximum<int> generic = new Maximum<int>();
+        generic.PrintMaxValue(intArray);
+    }
+}
